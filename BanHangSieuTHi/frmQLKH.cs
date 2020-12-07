9528ca2272a627939a67895c5ef2a4120d54ee3e
NguyenHuuHai2310 <56294670+NguyenHuuHai2310@users.noreply.github.com>
2020-12-07 12:05:41 +0700

    from quản lý khách hàng

---
+++ BanHangSieuTHi/frmQLKH.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using BanHangSieuTHi.Class;
+
+
+
+namespace BanHangSieuTHi
+{
+    public partial class frmQLKH : Form
+    {
+        public frmQLKH()
+        {
+            InitializeComponent();
+        }
+
+        string temp;
+        //Chưa làm
+        public void LoadListView()
+        {
+            
+        }
+        //Chưa làm
+        private void frmQLKH_Load(object sender, EventArgs e)
+        {
+            
+        }
+        //Chưa làm
+        private void listView1_ItemActivate(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void txtMa_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void txtTen_TextChanged(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void txtDiaChi_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void txtSdt_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        //Chưa làm
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            
+        }
+        //Chưa làm
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        //Chưa làm
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lbl4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+
+}
