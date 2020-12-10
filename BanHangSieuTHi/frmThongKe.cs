f25807ae9556fd0328a861d90ff7ba0ba7954056
ThanhHa0806 <riverandsnow300418@gmail.com>
2020-12-10 23:23:47 +0700

    Update module thống kê
    Hiển thị báo cáo thống kê

--- BanHangSieuTHi/frmThongKe.cs
+++ BanHangSieuTHi/frmThongKe.cs
@@ -67,7 +67,70 @@ namespace BanHangSieuTHi
 
         private void btnShow_Click(object sender, EventArgs e)
         {
-            
+            listViewNhap.Items.Clear();
+            listViewXuat.Items.Clear();
+            sqlQuery truyVanDL = new sqlQuery();
+            string addLoai, addNCC, MaLoai, MaNCC;
+            if (cbLoaiHang.SelectedIndex == -1)
+            {
+                addLoai = "";
+            }
+            else
+            {
+                DataTable dt2 = truyVanDL.LayDuLieu("select MaLoai from LOAIHANG where DienGiai=N'" + cbLoaiHang.SelectedItem.ToString() + "'");
+                MaLoai = dt2.Rows[0][0].ToString();
+                addLoai = " and c.MaLoai = '" + MaLoai + "'";
+            }
+            if (cbNhaCC.SelectedIndex == -1)
+            {
+                addNCC = "";
+            }
+            else
+            {
+                DataTable dt1 = truyVanDL.LayDuLieu("select MaNCC from NHACUNGCAP where TenNCC=N'" + cbNhaCC.SelectedItem.ToString() + "'");
+                MaNCC = dt1.Rows[0][0].ToString();
+                addNCC = " and hd.MaNCC='" + MaNCC + "'";
+            }
+            dateTimePicker1.ShowUpDown = true;
+            dateTimePicker2.ShowUpDown = true;
+
+
+
+            DataTable dt = truyVanDL.LayDuLieu("Select h.Mahang,h.TenHang,c.SoLuongNhap,c.DonGiaN from CHITIETNHAP as c , HANGHOA as h,HOADONNHAP as hd where c.SoHDN= hd.SoHDN and h.MaHang=c.MaHang and hd.NgayNhap between cast('" + dateTimePicker1.Value.ToString("yyyy/MM/dd") + "' as date) and cast('" + dateTimePicker2.Value.ToString("yyyy/MM/dd") + "' as date)" + addLoai + addNCC + "");
+            int soSPNhap = dt.Rows.Count, SLNhap = 0, thanhtien = 0;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ListViewItem item = new ListViewItem((i + 1).ToString());
+                item.SubItems.Add(dt.Rows[i][0].ToString());
+                item.SubItems.Add(dt.Rows[i][1].ToString());
+                item.SubItems.Add(dt.Rows[i][2].ToString());
+                int tien = Int16.Parse(dt.Rows[i][3].ToString()) * Int16.Parse(dt.Rows[i][3].ToString());
+                item.SubItems.Add(tien.ToString());
+                listViewNhap.Items.Add(item);
+                SLNhap = SLNhap + Int16.Parse(dt.Rows[i][2].ToString());
+                thanhtien = thanhtien + tien;
+            }
+            txtSoSPNhap.Text = soSPNhap.ToString();
+            txtSLNhap.Text = SLNhap.ToString();
+            txtTienNhap.Text = thanhtien.ToString();
+
+            DataTable dt4 = truyVanDL.LayDuLieu("Select h.Mahang,h.TenHang,c.SoLuong,c.DonGia from CHITIETXUAT as c , HANGHOA as h,HOADONXUAT as hd where c.SoHDX= hd.SoHDX and h.MaHang=c.MaHang and hd.NgayXuat between cast('" + dateTimePicker1.Value.ToString("yyyy/MM/dd") + "' as date) and cast('" + dateTimePicker2.Value.ToString("yyyy/MM/dd") + "' as date)" + addLoai + "");
+            int soSPBan = dt4.Rows.Count, SLBan = 0, thanhtien1 = 0;
+            for (int i = 0; i < dt4.Rows.Count; i++)
+            {
+                ListViewItem item = new ListViewItem((i + 1).ToString());
+                item.SubItems.Add(dt4.Rows[i][0].ToString());
+                item.SubItems.Add(dt4.Rows[i][1].ToString());
+                item.SubItems.Add(dt4.Rows[i][2].ToString());
+                int tien = Int16.Parse(dt4.Rows[i][3].ToString()) * Int16.Parse(dt4.Rows[i][3].ToString());
+                item.SubItems.Add(tien.ToString());
+                listViewXuat.Items.Add(item);
+                SLBan = SLBan + Int16.Parse(dt4.Rows[i][2].ToString());
+                thanhtien1 = thanhtien1 + tien;
+            }
+            txtSoSPBan.Text = soSPBan.ToString();
+            txtSLBan.Text = SLBan.ToString();
+            txtTienBan.Text = thanhtien1.ToString();
         }
             
         }
